1eaba06023f66394380e2ca725c2583d606b11c9
Cameron Falls <camfalls0201@gmail.com>
2020-06-06 16:23:33 -0500

    Update model structure

--- model.py
+++ model.py
@@ -1,15 +1,24 @@
+from random import random
+
 import numpy as np
 import gym
 
-GAMMA = 0.90
-APLHA = 0.01
+APLHA = 0.1 # Learning Rate
+GAMMA = 0.90 # Discount Factor
+EPSILON = 0.1 # Exploration Rate
 
 class Model:
     def __init__(self):
-        self.memory = np.array({})
+        self.memory = {} # {(observation space) : {action : reward}}
     
     def saveState(self, state):
-        self.memory = np.append(self.memory, state)
+        INITIAL_WEIGHT = 0.0
+
+        if state not in self.memory:
+            self.memory.update({state : {0 : INITIAL_WEIGHT, 1 : INITIAL_WEIGHT}})
     
-    def getAction(self, actionSpace):
-        return actionSpace.sample()
+    def getAction(self, actionSpace, state):
+        if random() > EPSILON:
+            return max(self.memory[state][0], self.memory[state][1])
+        else:
+            return actionSpace.sample()
